3c4e6e6d6d5ac0890987899d3dc6ef1cf6d47ca9
Alessandro Rossi <alessandro.rossi7610@gmail.com>
2021-05-21 10:02:24 +0200

    Modified api link to get all nearby postal code instead of 5 only

--- models/distanceCalculation.php
+++ models/distanceCalculation.php
@@ -1,8 +1,14 @@
 <?php
 
+/**
+ * This function will get neihbourg postal code from the user's postal code
+ * It use a public and free API from "api.geonames.org"
+ * @param  string $currentPC The user's postal code
+ * @return return an array of unique postal code
+ */
 function getNearbyPostalCode(string $currentPC){
   $nearPC = array();
-  $url = "http://api.geonames.org/findNearbyPostalCodes?postalcode=".$currentPC."&country=CH&radius=30&username=havachi";
+  $url = "http://api.geonames.org/findNearbyPostalCodes?postalcode=".$currentPC."&country=CH&username=havachi&maxRows=500&radius=30";
   if (!function_exists('curl_init')){
     die('Sorry cURL is not installed!');
   }
